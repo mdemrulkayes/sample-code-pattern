1e9b09a7726b66fe44aa89cd2683540b16921be9
Emrul Kayes <emrul@blockbonds.io>
2022-11-07 17:08:27 +0600

    added fluent validaton command

---
+++ Application/Employee/CreateEmployee/CreateEmployeeCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Application.Employee.CreateEmployee
+{
+    public class CreateEmployeeCommandValidator: AbstractValidator<CreateEmployeeCommand>
+    {
+        public CreateEmployeeCommandValidator()
+        {
+            RuleFor(x => x.Name).MaximumLength(100).NotEmpty();
+        }
+    }
+}
